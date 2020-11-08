773d36949c57f0bcfd0d99b48da92cf7683c8952
MS-MS-MS <13439718124@163.com>
2020-11-08 17:18:39 +0800

    2020 1108 pytest1 作业

---
+++ pythoncode/calculator.py
@@ -0,0 +1,17 @@
+"""
+计算器 加减乘除
+2020 11 .8
+ms
+"""
+class Calculator:
+    def add(self,a,b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
